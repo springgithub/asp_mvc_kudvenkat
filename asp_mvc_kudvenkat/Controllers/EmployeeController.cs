5815abc3dccb88e7e6d3f0120dd7f0519ba64010
dac5934 <jairoting@hotmail.com>
2020-12-04 08:08:40 -0500

    Part 7 Models in an mvc application

---
+++ asp_mvc_kudvenkat/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using asp_mvc_kudvenkat.Models;
+
+namespace asp_mvc_kudvenkat.Controllers
+{
+    public class EmployeeController: Controller
+    {
+
+        public ActionResult Details()
+        {
+            Employee x = new Employee()
+            {
+                EmployeeId = 12,
+                Name = "John",
+                Gender = "Male",
+                City = "Twin peaks"
+            };
+            return View(x);
+
+        }
+    }
+}
